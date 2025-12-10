ce0fdcbe8c853c9acc3c1a1e21d79a5401162ed6
Keanu Geronimo <keanugeronimo@gmail.com>
2025-12-11 07:37:40 +0800

    Updated remote encoder to match testing behavior

--- lib/ACU_remote_encoder/ACU_remote_encoder.cpp
+++ lib/ACU_remote_encoder/ACU_remote_encoder.cpp
@@ -139,9 +139,13 @@ uint8_t ACU_remote::encodeSignature() const {
 // Fan speed encoded as 4 bits
 uint8_t ACU_remote::encodeFanSpeed() const {
   switch (state.fanSpeed) {
-    case 1: return 0b0010;
-    case 2: return 0b1010;
-    case 3: return 0b0110;
+    case 1: return 0b0000;
+    case 2: return 0b1000;
+    case 3: return 0b0100;
+
+    case 4: return 0b0010; // Swing
+    case 5: return 0b1010;
+    case 6: return 0b0110;
     // case 4: return 0b1110; // Triple beeps
     default: return 0b0000;
   }
@@ -186,28 +190,16 @@ uint8_t ACU_remote::encodeMode() const {
 // Encode louver position as 4-bit value
 uint8_t ACU_remote::encodeLouver() const {
   switch (state.louver) {
-    case 0: return 0b0010; // 22.5 deg
-    case 1: return 0b1010; // 45 deg
-    case 2: return 0b0110; // 67.5
-    case 3: return 0b1110; // 0 deg
+    case 0: return 0b0010; // 0 deg
+    case 1: return 0b1010; // 22.5 deg
+    case 2: return 0b0110; // 45 deg
+    case 3: return 0b1110; // 67.5 deg
     case 4: return 0b0000; // swing
-    // case 0: return 0b0000;
-    // case 1: return 0b0001;
-    // case 2: return 0b0010;
     // case 3: return 0b0011; // Triple beeps - not encoded
-    // case 4: return 0b0100; // Same pattern
-    // case 5: return 0b0101; // Only 00XX is encoded
-    // case 6: return 0b0110;
     // case 7: return 0b0111; // Triple beeps - not encoded
-    // case 8: return 0b1000;
-    // case 9: return 0b1001;
-    // case 10: return 0b1010; 
     // case 11: return 0b1011; // Triple beeps
-    // case 12: return 0b1100;
-    // case 13: return 0b1101;
-    // case 14: return 0b1110;
     // case 15: return 0b1111; // Triple beeps
-    default: return 0b1110; // 0 deg default
+    default: return 0b0010; // 0 deg default
   }
 }
 
